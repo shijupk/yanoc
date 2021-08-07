f0e263b3585997e7a10d613a47fffb810bc18d50
Shiju P K <Shiju.PK@philips.com>
2021-08-08 02:22:23 +0530

    Function support

--- YanoC/common.h
+++ YanoC/common.h
@@ -5,7 +5,9 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define DEBUG_PRINT_CODE
-#define DEBUG_TRACE_EXECUTION
+//#define DEBUG_PRINT_CODE
+//#define DEBUG_TRACE_EXECUTION
+
+#define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif
--- YanoC/vm.c
+++ YanoC/vm.c
@@ -114,6 +114,9 @@ InterpretResult run()
 
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
+#define READ_SHORT() \
+    (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
+
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 
 #define BINARY_OP(valueType, op) \
@@ -157,6 +160,18 @@ InterpretResult run()
     case OP_TRUE: push(BOOL_VAL(true)); break;
     case OP_FALSE: push(BOOL_VAL(false)); break;
     case OP_POP: pop(); break;
+    case OP_GET_LOCAL:
+    {
+      uint8_t slot = READ_BYTE();
+      push(vm.stack[slot]);
+      break;
+    }
+    case OP_SET_LOCAL:
+    {
+      uint8_t slot = READ_BYTE();
+      vm.stack[slot] = peek(0);
+      break;
+    }
     case OP_GET_GLOBAL:
     {
       ObjString* name = READ_STRING();
@@ -240,6 +255,24 @@ InterpretResult run()
       printf("\n");
       break;
     }
+    case OP_JUMP:
+    {
+      uint16_t offset = READ_SHORT();
+      vm.ip += offset;
+      break;
+    }
+    case OP_JUMP_IF_FALSE:
+    {
+      uint16_t offset = READ_SHORT();
+      if (isFalsey(peek(0))) vm.ip += offset;
+      break;
+    }
+    case OP_LOOP:
+    {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
+      break;
+    }
     case OP_RETURN:
     {
       //exit interpreter
@@ -252,6 +285,7 @@ InterpretResult run()
 ////////////////////////////////////////////////////////////////////////////////
 
 #undef READ_BYTE;
+#undef READ_SHORT
 #undef READ_CONSTANT
 #undef READ_STRING
 #undef BINARY_OP
