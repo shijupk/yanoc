6ac7c722a7b416e4b218e5b2863c99a5f8dddf5d
Shiju P K <Shiju.PK@philips.com>
2021-08-09 00:15:57 +0530

    added class

--- YanoC/memory.c
+++ YanoC/memory.c
@@ -91,6 +91,20 @@ static void blackenObject(Obj* object)
 
   switch (object->type)
   {
+  case OBJ_BOUND_METHOD:
+  {
+    ObjBoundMethod* bound = (ObjBoundMethod*)object;
+    markValue(bound->receiver);
+    markObject((Obj*)bound->method);
+    break;
+  }
+  case OBJ_CLASS:
+  {
+    ObjClass* klass = (ObjClass*)object;
+    markObject((Obj*)klass->name);
+    markTable(&klass->methods);
+    break;
+  }
   case OBJ_CLOSURE:
   {
     ObjClosure* closure = (ObjClosure*)object;
@@ -106,6 +120,13 @@ static void blackenObject(Obj* object)
     ObjFunction* function = (ObjFunction*)object;
     markObject((Obj*)function->name);
     markArray(&function->chunk.constants);
+    break;
+  }
+  case OBJ_INSTANCE:
+  {
+    ObjInstance* instance = (ObjInstance*)object;
+    markObject((Obj*)instance->klass);
+    markTable(&instance->fields);
     break;
   }
   case OBJ_UPVALUE:
@@ -125,6 +146,17 @@ static void freeObject(Obj* object)
 
   switch (object->type)
   {
+  case OBJ_BOUND_METHOD:
+    FREE(ObjBoundMethod, object);
+    break;
+  case OBJ_CLASS:
+  {
+    ObjClass* klass = (ObjClass*)object;
+    freeTable(&klass->methods);
+
+    FREE(ObjClass, object);
+    break;
+  }
   case OBJ_CLOSURE:
   {
     ObjClosure* closure = (ObjClosure*)object;
@@ -137,6 +169,13 @@ static void freeObject(Obj* object)
     ObjFunction* function = (ObjFunction*)object;
     freeChunk(&function->chunk);
     FREE(ObjFunction, object);
+    break;
+  }
+  case OBJ_INSTANCE:
+  {
+    ObjInstance* instance = (ObjInstance*)object;
+    freeTable(&instance->fields);
+    FREE(ObjInstance, object);
     break;
   }
   case OBJ_NATIVE:
@@ -176,6 +215,7 @@ static void markRoots()
 
   markTable(&vm.globals);
   markCompilerRoots();
+  markObject((Obj*)vm.initString);
 
 }
 
